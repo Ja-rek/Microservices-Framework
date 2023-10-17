5afde1a4a16d71524f6d88f8bbd5a045a448888e
Ja-Rek <dto.jarek@gmail.com>
2023-10-17 13:56:16 +0800

    metrics refactor

--- MicroservicesFramework.Metrics/MetricsExtensions.cs
+++ MicroservicesFramework.Metrics/MetricsExtensions.cs
@@ -2,16 +2,19 @@
 using App.Metrics.AspNetCore;
 using App.Metrics.Formatters.Prometheus;
 using MicroservicesFramework.Metrics.Options;
+using MicroservicesFramework.Common;
 
-namespace MicroservicesFramework.Logging;
+namespace MicroservicesFramework.Metrics;
 
 public static class MetricsExtensions
 {
-    public static WebApplicationBuilder UseMetrics(this WebApplicationBuilder builder, MetricOption metricOptions)
+    public static WebApplicationBuilder UseMetrics(this WebApplicationBuilder builder)
     {
+        var metricOptions = builder.Configuration.GetOptions<MetricOption>("tracing");
+
         if (metricOptions is not null && metricOptions.Enabled)
         {
-            builder.Host.UseMetrics( options =>
+            builder.Host.UseMetrics(options =>
             {
                 if (metricOptions.EnablePrometheus)
                 {
